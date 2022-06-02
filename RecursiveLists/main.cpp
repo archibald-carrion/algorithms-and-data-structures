fb0714214536c5a0e21de427ad57bb2d17d2ed16
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-02 01:48:17 -0600

    bug fixing

--- RecursiveLists/main.cpp
+++ RecursiveLists/main.cpp
@@ -6,30 +6,30 @@ int main(int args, char **argv){
     string inputFile = argv[1];
     string outputFile = argv[2];
     
-    Lista *lista = new Lista();
+    Lista lista;
     int data;
     ifstream documentoInput(inputFile);
     if(!(documentoInput.is_open())){
         cout<<"problema con la apertura del archivo de texto"<<endl;
     } 
     documentoInput>>data;
-    (*lista).insertar(data);    //para "recibir" el primer data
+    lista.insertar(data);    //para "recibir" el primer data
 	while(!documentoInput.eof()) {
         documentoInput>>data;
         //cout<<data<<endl;
-        (*lista).insertar(data);
+        lista.insertar(data);
 	}
     documentoInput.close();
 
-    cout<<(*lista)<<"\n"<<endl;
-    int sizeLista = (*lista).getSize();
+    cout<<lista<<"\n"<<endl;
+    int sizeLista = lista.getSize();
     int counter = 0;
-    Lista *listaFrecuencia = new Lista();
+    Lista listaFrecuencia;
     while(counter<sizeLista){
-        (*listaFrecuencia).insertar(lista->getFrecuencia(counter));
+        listaFrecuencia.insertar(lista.getFrecuencia(counter));
         ++counter;
     }
-    cout<<(*listaFrecuencia)<<endl;
+    cout<<listaFrecuencia<<endl;
     
     ofstream documentoOuput(outputFile);
     if(documentoOuput.is_open()){
@@ -38,14 +38,15 @@ int main(int args, char **argv){
         cout<<"problema con la apertura del archivo de texto"<<endl;
     }
 
-    int sizeFrecuencia = (*listaFrecuencia).getSize();
+    int sizeFrecuencia = listaFrecuencia.getSize();
     counter = 0;
     while(counter<sizeFrecuencia){
-        documentoOuput << (*listaFrecuencia).get(counter) << "\t\t" << (*listaFrecuencia).getFrecuencia(counter)<<"\n";
+        documentoOuput << listaFrecuencia.get(counter) << "\t\t" << listaFrecuencia.getFrecuencia(counter)<<"\n";
+        ++counter;
     }
 
     documentoOuput.close();
-    delete listaFrecuencia;
-    delete lista;
+    //delete listaFrecuencia;
+    //delete lista;
     return 0;
 }
