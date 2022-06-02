2853b84d91fe3dd4a3b9a9946962575c1ffc273c
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-01 21:33:13 -0600

    main.cpp dev

--- RecursiveLists/main.cpp
+++ RecursiveLists/main.cpp
@@ -1,41 +1,27 @@
 #include "Lista.h"
 
 int main(){ 
-    Lista *lista1 = new Lista();;   //lista por omisión
-    int numeros[6]={1,4,3,2,1,-2};     
-    Lista lista2(6,numeros);        //lista por parametros
-    Lista lista3(lista2);           //lista por copia
-    Lista *lista4 = new Lista();    //lista nula
-
-    *lista1 = lista2;
-    //lista3 = (*lista4);
-    lista2.insertar(10);
-    lista2.insertar(1);
-    lista2.insertar(-10); //SIGO TENIENDO EL PROBLEMA DE INSERTAR UNA ELEMENTO MAS PEQUEÑO
-
-    cout<<"lista1 :"<<endl;
-    cout<<*lista1<<"\n"<<endl;
-    cout<<"lista2 :"<<endl;
-    cout<<lista2<<"\n"<<endl;
-    cout<<"lista3 :"<<endl;
-    cout<<lista3<<"\n"<<endl;
-    cout<<"lista4 :"<<endl;
-    cout<<*lista4<<"\n"<<endl;
-
-    cout<<"lista1.isEmpty = "<<(*lista1).isEmpty()<<endl;
-    cout<<"lista2.isEmpty = " << lista2.isEmpty()<<endl;
-    cout<<"lista3.isEmpty = " << lista3.isEmpty()<<endl;
-    cout<<"lista4.isEmpty = " <<(*lista4).isEmpty()<<"\n"<<endl;
+    //como descrito en el enunciado, se recibe un archivo de texto con la estructura deseada
+    //lo cual es recibir cada dato en una linea differente, ya que voy a utilizar la función 
     
-    int numero = lista2==(*lista1);
-    cout<<"lista2==(*lista1);  is iqual to: " << numero<<"\n"<<endl;
-
-    int posicion = 0;
-    cout<<"posicion: "<<posicion<<endl;
-    cout<<"el_valor: " << lista2.get(posicion)<<endl;
-    cout<<"su_frecuencia: " << lista2.getFrecuencia(posicion)<<endl;
-    
-    delete lista1;
-    delete lista4;
+    //primero hay que abrir el archivo de texto
+    //se crea una lista por omision
+    //en un ciclo se agrega cada elemento en la lista, linea por linea
+    //despues se llama cada frecuencia y se saca la frecuencia de la frecuencia
+    Lista *lista = new Lista();
+    char* archivoTexto = (char*)"datos.txt";
+    FILE *miDocumento;
+	miDocumento = fopen(archivoTexto,"r+t");
+	//int contador = 0;
+    char* input;
+    int finArchivo = 0;
+	//el while permite reiterar la cantidad de veces necesarias para sacar la linea necesitada
+	while(finArchivo != 1) {
+		//hileraLinea[0]='\0';
+		fgets(input, 50, miDocumento);
+        
+        //hay que poner un if para saber si es el fin del archivos
+	}
+	fclose(miDocumento);
     return 0;
 }
--- RecursiveLists/main_TEST/main1.cpp
+++
@@ -1,8 +0,0 @@
-#include "Lista.h"
-
-int main(){ 
-    //como descrito en el enunciado, se recibe un archivo de texto con la estructura deseada
-    //lo cual es recibir cada dato en una linea differente, ya que voy a utilizar la función 
-    
-    return 0;
-}
