9f5ce37d1fecca0524cf9d627400b3c976521581
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-15 17:24:02 -0600

    added Elemento.h content

--- PolymorphicBinaryTree/Elemento.h
+++ PolymorphicBinaryTree/Elemento.h
@@ -1,8 +1,27 @@
-#ifndef ABSTRACT_ELEMENTO_
-#define ABSTRACT_ELEMENTO_
+#ifndef ELEMENTO
+#define ELEMENTO
+#include<iostream>
+using namespace std;
 
-class Elemento{
-
+class Elemento {
+   friend ostream& operator<<(ostream& salida,Elemento * p){
+      return p->imprimir(salida);  
+   }
+   
+   friend istream& operator>>(istream& entrada,Elemento * p){
+      return p->leer(entrada);  
+   }   
+   
+   public:
+      virtual ~Elemento(){};
+      virtual Elemento * clonar()=0;
+	  virtual ostream& imprimir(ostream&)=0;
+      virtual istream& leer(istream&)=0;
+	  virtual int operator==(Elemento *)=0;
+	  virtual int operator!=(Elemento *)=0;
+	  virtual int operator<(Elemento *)=0;
 
 };
+
+
 #endif
