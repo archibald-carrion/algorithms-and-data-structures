7435920bdc30f803171d0882c598a995d49878f2
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-15 17:32:52 -0600

    added content of the main

--- PolymorphicBinaryTree/main.cpp
+++ PolymorphicBinaryTree/main.cpp
@@ -1,1 +1,27 @@
-//main of the linked list
+#include "Arbol.h"
+#include "Elemento.h"
+#include "Letra.h"
+#include <iostream>
+using namespace std;
+int main(){
+   Arbol arbol;
+//   Elemento * elemento = new Entero();
+   Elemento * elemento = new Letra();
+   
+   for(int i=0;i < 10; ++i){
+      cout << "Digite un elemento: " ;
+	  cin >> elemento;
+	  arbol.add(elemento);
+	  cout << endl;
+   }
+   cout << "ELEMENTOS"<<endl;
+   cout << arbol;
+   cout << endl;
+   do{
+      cout << "Digite elemento existente: " ;
+	  cin >> elemento;
+	  cout << endl;   
+   }while(arbol.existe(elemento));
+   cout << "Ese valor "<< elemento << " no existe "<< endl;
+   return 0;
+}
