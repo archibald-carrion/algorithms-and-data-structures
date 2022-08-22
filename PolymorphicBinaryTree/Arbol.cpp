af7d6fa9a19769bbccd0217a953930c6e45dc2b4
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-08-22 10:59:29 -0600

    a few layout changes

--- PolymorphicBinaryTree/Arbol.cpp
+++ PolymorphicBinaryTree/Arbol.cpp
@@ -1,96 +1,122 @@
 #include "Arbol.h"
 #include "Elemento.h"
-#include<iostream>
+#include <iostream>
 
-Arbol::Nodo::Nodo(Elemento * elemento){
-   this->elemento = elemento->clonar();
-   hijo[IZQ]=0;   
-   hijo[DER]=0;
+Arbol::Nodo::Nodo(Elemento *elemento)
+{
+	this->elemento = elemento->clonar();
+	hijo[IZQ] = 0;
+	hijo[DER] = 0;
 }
 
-Arbol::Nodo::~Nodo(){
-  if(hijo[IZQ]){
-	delete hijo[IZQ];  
-  }
-  if(elemento){
-	  delete elemento;
-  }
-  if(hijo[DER]){
-	delete hijo[DER];  
-  }
+Arbol::Nodo::~Nodo()
+{
+	if (hijo[IZQ])
+	{
+		delete hijo[IZQ];
+	}
+	if (elemento)
+	{
+		delete elemento;
+	}
+	if (hijo[DER])
+	{
+		delete hijo[DER];
+	}
 }
 
-ostream& Arbol::Nodo::imprimir(ostream& salida){ 
-  	if(hijo[IZQ]){
-	  	hijo[IZQ]->imprimir(salida);
-  	}
-  	salida << elemento << " ";
-  
-  	if(hijo[DER]){
-	  	hijo[DER]->imprimir(salida);
-  	}	
+ostream &Arbol::Nodo::imprimir(ostream &salida)
+{
+	if (hijo[IZQ])
+	{
+		hijo[IZQ]->imprimir(salida);
+	}
+	salida << elemento << " ";
+
+	if (hijo[DER])
+	{
+		hijo[DER]->imprimir(salida);
+	}
 	return salida;
 }
 
-void Arbol::Nodo::add(Elemento * elemento){
-	if(*(this->elemento)!=elemento){
-		int lado = (*elemento < this->elemento)?IZQ:DER;
-		if(hijo[lado]){
+void Arbol::Nodo::add(Elemento *elemento)
+{
+	if (*(this->elemento) != elemento)
+	{
+		int lado = (*elemento < this->elemento) ? IZQ : DER;
+		if (hijo[lado])
+		{
 			hijo[lado]->add(elemento);
 		}
-		else {
+		else
+		{
 			hijo[lado] = new Nodo(elemento);
 		}
 	}
 }
 
-int Arbol::Nodo::existe(Elemento * elemento){
+int Arbol::Nodo::existe(Elemento *elemento)
+{
 	int existeElemento = 0;
-	if(*(this->elemento)==elemento){
+	if (*(this->elemento) == elemento)
+	{
 		existeElemento = 1;
 	}
-	else {
-		int lado = (*elemento < this->elemento)?IZQ:DER;
-		if(hijo[lado]){
+	else
+	{
+		int lado = (*elemento < this->elemento) ? IZQ : DER;
+		if (hijo[lado])
+		{
 			existeElemento = hijo[lado]->existe(elemento);
-		}	
+		}
 	}
 	return existeElemento;
 }
 
-ostream& Arbol::imprimir(ostream&salida){
-	if(raiz){
+ostream &Arbol::imprimir(ostream &salida)
+{
+	if (raiz)
+	{
 		raiz->imprimir(salida);
 	}
-	else {
-		salida << "ARBOL VACIO"<< endl;
+	else
+	{
+		salida << "ARBOL VACIO" << endl;
 	}
 	return salida;
 }
 
-Arbol::Arbol(){
+Arbol::Arbol()
+{
 	raiz = 0;
 }
 
-Arbol::~Arbol(){
-	if(raiz){
-	   delete raiz;	
+Arbol::~Arbol()
+{
+	if (raiz)
+	{
+		delete raiz;
 	}
-	
 }
 
-void Arbol::add(Elemento * elemento){
-	if(raiz){
+void Arbol::add(Elemento *elemento)
+{
+	if (raiz)
+	{
 		raiz->add(elemento);
 	}
-	else{
-		raiz= new Nodo(elemento);
+	else
+	{
+		raiz = new Nodo(elemento);
 	}
 }
 
-int Arbol::existe(Elemento * elemento){
+int Arbol::existe(Elemento *elemento)
+{
 	int existeElemento = 0;
-	if(raiz){
+	if (raiz)
+	{
 		existeElemento = raiz->existe(elemento);
 	}
 	return existeElemento;
--- PolymorphicBinaryTree/Arbol.h
+++ PolymorphicBinaryTree/Arbol.h
@@ -3,31 +3,36 @@
 #define IZQ 0
 #define DER 1
 #define HIJOS 2
-#include<iostream>
+#include <iostream>
 #include "Elemento.h"
 using namespace std;
-class Arbol {
-   friend ostream& operator<<(ostream& salida, Arbol & arbol){
-      return arbol.imprimir(salida);
-   }
-   private:
-      class Nodo {
-	     public:
-		    Elemento * elemento;
-	        Nodo * hijo[HIJOS];
-			Nodo(Elemento *);
-			~Nodo();
-			ostream& imprimir(ostream&);
-			void add(Elemento *);
-			int existe(Elemento *);
-	  };
+class Arbol
+{
+	friend ostream &operator<<(ostream &salida, Arbol &arbol)
+	{
+		return arbol.imprimir(salida);
+	}
 
-      Nodo * raiz;
-	  ostream& imprimir(ostream&);
-    public:
-	    Arbol();
-		~Arbol();
+private:
+	class Nodo
+	{
+	public:
+		Elemento *elemento;
+		Nodo *hijo[HIJOS];
+		Nodo(Elemento *);
+		~Nodo();
+		ostream &imprimir(ostream &);
 		void add(Elemento *);
 		int existe(Elemento *);
+	};
+
+	Nodo *raiz;
+	ostream &imprimir(ostream &);
+
+public:
+	Arbol();
+	~Arbol();
+	void add(Elemento *);
+	int existe(Elemento *);
 };
 #endif
