e10d48f489aee0bfa2d4455bdc1e64ef2ddfa0c0
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-08-22 11:00:10 -0600

    layout changes in Elemento.h

--- PolymorphicBinaryTree/Elemento.h
+++ PolymorphicBinaryTree/Elemento.h
@@ -1,27 +1,28 @@
 #ifndef ELEMENTO
 #define ELEMENTO
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Elemento {
-   friend ostream& operator<<(ostream& salida,Elemento * p){
-      return p->imprimir(salida);  
+class Elemento
+{
+   friend ostream &operator<<(ostream &salida, Elemento *p)
+   {
+      return p->imprimir(salida);
    }
-   
-   friend istream& operator>>(istream& entrada,Elemento * p){
-      return p->leer(entrada);  
-   }   
-   
-   public:
-      virtual ~Elemento(){};
-      virtual Elemento * clonar()=0;
-	   virtual ostream& imprimir(ostream&)=0;
-      virtual istream& leer(istream&)=0;
-	   virtual int operator==(Elemento *)=0;
-	   virtual int operator!=(Elemento *)=0;
-	   virtual int operator<(Elemento *)=0;
 
+   friend istream &operator>>(istream &entrada, Elemento *p)
+   {
+      return p->leer(entrada);
+   }
+
+public:
+   virtual ~Elemento(){};
+   virtual Elemento *clonar() = 0;
+   virtual ostream &imprimir(ostream &) = 0;
+   virtual istream &leer(istream &) = 0;
+   virtual int operator==(Elemento *) = 0;
+   virtual int operator!=(Elemento *) = 0;
+   virtual int operator<(Elemento *) = 0;
 };
-
 
 #endif
