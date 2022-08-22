e5edfc8fcce209cbb2550ee9fb461529fc3561bb
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-08-22 11:03:19 -0600

    update in USer.h layout

--- PolymorphicBinaryTree/User.h
+++ PolymorphicBinaryTree/User.h
@@ -1,22 +1,22 @@
 #ifndef _USER_
 #define _USER_
 #include "Elemento.h"
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class User: public Elemento {
-   private:
-      unsigned long int id;               //de 0 a 4 294 967 295
-   public:
-      User();
-	   User(unsigned long int);
-      Elemento * clonar();
-      ostream& imprimir(ostream&);
-      istream& leer(istream&);
-      int operator==(Elemento *);
-      int operator!=(Elemento *);
-      int operator<(Elemento *);
+class User : public Elemento
+{
+private:
+   unsigned long int id; // de 0 a 4 294 967 295
+public:
+   User();
+   User(unsigned long int);
+   Elemento *clonar();
+   ostream &imprimir(ostream &);
+   istream &leer(istream &);
+   int operator==(Elemento *);
+   int operator!=(Elemento *);
+   int operator<(Elemento *);
 };
-
 
 #endif
