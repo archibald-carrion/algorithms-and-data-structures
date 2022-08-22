6d8e0e71881a44322812ddccd4fd1947069490cc
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-08-22 13:57:08 -0600

    added sorting algorithm file

---
+++ SortingAlgorithms/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+void bubblesort(deque<string> deck){
+
+
+
+}
+
+
+void quicksort(deque<string> deck){
+
+
+
+
+
+}
+
+
+void mergesortrec(deque<string> deck){
+
+
+
+
+}
+
+
+void mergesort(deque<string> deck){
+
+
+
+
+
+}
+
+int main(){
+
+
+
+
+
+	return 0;
+}
+
+
+
