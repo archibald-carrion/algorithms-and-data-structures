1ecdd097c6d3e0c53d69f909098e230ef6fc002a
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-08-22 11:01:22 -0600

    update in layout of User.cpp

--- PolymorphicBinaryTree/User.cpp
+++ PolymorphicBinaryTree/User.cpp
@@ -1,39 +1,47 @@
 #include "Elemento.h"
 #include "User.h"
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-User::User(){
+User::User()
+{
 	id = 0;
 }
-User::User(unsigned long int idUser){
+User::User(unsigned long int idUser)
+{
 	this->id = idUser;
 }
 
-Elemento * User::clonar(){
+Elemento *User::clonar()
+{
 	return new User(id);
 }
 
-ostream& User::imprimir(ostream& salida){
+ostream &User::imprimir(ostream &salida)
+{
 	salida << id << " ";
 	return salida;
 }
 
-istream& User::leer(istream& entrada){
+istream &User::leer(istream &entrada)
+{
 	entrada >> id;
 	return entrada;
 }
 
-int User::operator==(Elemento * otro){
-	User * otroUser = dynamic_cast<User *>(otro);
-	return otroUser && id==otroUser->id;
+int User::operator==(Elemento *otro)
+{
+	User *otroUser = dynamic_cast<User *>(otro);
+	return otroUser && id == otroUser->id;
 }
 
-int User::operator!=(Elemento * otro){
-    return !((*this)==otro);
+int User::operator!=(Elemento *otro)
+{
+	return !((*this) == otro);
 }
 
-int User::operator<(Elemento * otro){
-	User * otroUser = dynamic_cast<User *>(otro);
-	return otroUser && this->id < otroUser->id;		  		  
+int User::operator<(Elemento *otro)
+{
+	User *otroUser = dynamic_cast<User *>(otro);
+	return otroUser && this->id < otroUser->id;
 }
